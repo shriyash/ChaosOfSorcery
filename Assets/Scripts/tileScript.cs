1de6da815049f1fe39320fbbd4231ecc28001590
Broski-AC <acbroski@oakland.edu>
2020-03-17 22:09:56 -0400

    DefenseGame UI Update
    Added grid and color-changing tiles

---
+++ Assets/Scripts/tileScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileScript : MonoBehaviour
+{
+
+    Renderer colorRenderer;
+    private Color regularColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       regularColor = this.gameObject.GetComponent<Renderer>().material.GetColor("_Color");;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Changes the color of the box on hover
+    void OnMouseEnter(){
+        colorRenderer = this.gameObject.GetComponent<Renderer>();
+        //Can change the color here
+        colorRenderer.material.SetColor("_Color", Color.yellow);
+    }
+
+    //Resets color to original color
+    void OnMouseExit(){
+        colorRenderer.material.SetColor("_Color", regularColor);
+    }
+}
