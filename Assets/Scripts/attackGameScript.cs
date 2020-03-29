d9c98d918bcd8e89cf6d4d986bc1c70fb3d1b9c0
Broski-AC <acbroski@oakland.edu>
2020-03-29 00:40:43 -0400

    AttackGame UI
    Added UI for attack game

---
+++ Assets/Scripts/attackGameScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class attackGameScript : MonoBehaviour
+{
+    public Text pHealth;
+
+    public Text eHealth;
+
+    public Text pSpellSel;
+
+    public Text eSpellSel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
