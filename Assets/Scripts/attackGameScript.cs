cdf477557dee7ddf15e9d4ea46b5f092131cd519
James Lynott <41302574+jcelynott@users.noreply.github.com>
2020-03-31 04:42:44 -0400

    Added human and player selection mechanics

--- Assets/Scripts/attackGameScript.cs
+++ Assets/Scripts/attackGameScript.cs
@@ -12,13 +12,42 @@ public class attackGameScript : MonoBehaviour
     public Text pSpellSel;
 
     public Text eSpellSel;
+
+    public SpellHolder playerSelectedSpell, enemySelectedSpell;
+
+    public static attackGameScript attackDataInstance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        attackDataInstance = this;
+        if (BattleData.battleDatInstance != null) 
+        {
+            foreach (GameObject go in GameObject.FindGameObjectsWithTag("Spell RPS")) 
+            {
+                go.SetActive(false);
+                foreach (SpellHolder s in BattleData.battleDatInstance.playerSpells)
+                {
+                    if (go.GetComponent<SpellDataWrapper>().level == s.GetSpellLevel() && go.GetComponent<SpellDataWrapper>().spellType == s.GetSpellType()) 
+                    {
+                        go.SetActive(true);
+                    }
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
+    public void MakeEnemySelection() 
+    {
+        if (BattleData.battleDatInstance != null && BattleData.battleDatInstance.enemySpells != null) 
+        {
+            if (BattleData.battleDatInstance.enemySpells.Count > 0) 
+            {
+                enemySelectedSpell = BattleData.battleDatInstance.enemySpells.ToArray()[Random.Range(0, BattleData.battleDatInstance.enemySpells.Count)];
+
+            }
+        }
+    }
+
     void Update()
     {
         
