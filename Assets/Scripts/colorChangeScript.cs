1edcea981bccbecd0d56f77a2c646d4358a2aa61
Broski-AC <acbroski@oakland.edu>
2020-04-05 01:51:04 -0400

    Styling
    Made it look prettier

---
+++ Assets/Scripts/colorChangeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Refernce: https://docs.unity3d.com/ScriptReference/Camera-backgroundColor.html
+
+public class colorChangeScript : MonoBehaviour
+{
+    public Color color1;
+    public Color color2;
+    public float duration;
+
+    public Camera cam;
+
+    void Start()
+    {
+        cam.clearFlags = CameraClearFlags.SolidColor;
+    }
+
+    void Update()
+    {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        cam.backgroundColor = Color.Lerp(color1, color2, t);
+    }
+}
